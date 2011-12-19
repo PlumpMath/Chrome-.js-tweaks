a21962a06ca537f80d8c87b704552fd61ab64331
Magnar Sveen <magnars@gmail.com>
2011-12-19 11:22:43 +0100

    No yellow background on private repos.

---
+++ github.com.js
@@ -0,0 +1,6 @@
+
+
+// no yellow background on private repos please
+
+$("<style>ul.repo_list .private {background: url('../../images/icons/private.png') 10px 8px no-repeat #fff !important;}</style>").
+  appendTo(document.body);
