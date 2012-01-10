731cc14f66d37c9c78bc712f6428c972b0d4807f
Magnar Sveen <magnars@gmail.com>
2012-01-10 19:01:32 +0100

    More readable entries in google reader.

---
+++ google.com.js
@@ -0,0 +1,4 @@
+// google reader, y u so ugly
+
+$("<style>#chrome-viewer #viewer-container #entries .entry-main {font-size: 17px; background: #f4f4f4; padding: 20px;}</style>").
+  appendTo(document.body);
