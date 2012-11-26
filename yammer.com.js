5b923811a9cd5b53bf17230f55bb59bd3c3bdb25
Magnar Sveen <magnars@gmail.com>
2012-11-26 11:03:58 +0100

    Yammer: Remove stupid inbox + count in title.

--- yammer.com.js
+++ yammer.com.js
@@ -2,4 +2,10 @@
 
 // hide the useless sidebar
 
-$("<style>#sidebar {display: none !important;}</style>").appendTo(document.body);
+$("<style>#sidebar, #uni-messages {display: none !important;}</style>").appendTo(document.body);
+
+function fixTitle() { document.title = "Yammer"; }
+
+for (var i = 0; i < 5000; i += 50) {
+  setTimeout(fixTitle, i);
+}
